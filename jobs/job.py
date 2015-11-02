Fixed PPX calculation

jobs/job.py
@@ -89,24 +89,29 @@ class Job(ComponentsLoader):
 
 
 
-    def evaluate_dataset_lm(self, dataset_id, num=20):
+    def evaluate_dataset(self, dataset_id, num=20):
 
         self.loadTestMapper(dataset_id, num)
+        # self.X_tewt = self.mapper_test.X
+        # self.Y_test = self.mapper_test.Y
 
+        print "Calculating ppx as single sequence.."
         ppxConcat = self.perplexicity_sequence(self.X_test.ravel())
 
-        loss = self.model.model.evaluate(self.X_test, self.Y_test)
-
         print Fore.GREEN, "\nPerplexicity %.2f\n" % ppxConcat
+
+        loss, acc = self.model.evaluate(self.X_test, self.Y_test)
+
         print Fore.CYAN, "\nPerplexicity from LOSS %.2f\n" % np.exp(loss)
 
-
+        return
         ppxs = []
         # import pdb; pdb.set_trace()
 
+        print "Calculating ppx for all sequences.."
         for i in range(self.X_test.shape[0]):
             ppx = self.perplexicity_sequence(self.X_test[i])
-            print ppx
+            # print ppx
             ppxs.append(ppx)
 
         ppxs = np.array(ppxs)
@@ -119,7 +124,7 @@ class Job(ComponentsLoader):
 
         return out
 
-    def evaluate_dataset(self, dataset_id, num=20):
+    def evaluate_dataset_classify(self, dataset_id, num=20):
 
         self.loadTestMapper(dataset_id, num)
 
@@ -158,7 +163,7 @@ class Job(ComponentsLoader):
         X = np.delete(X,0,1)
         return self.perplexicity_sequence(X[0])
 
-    def perplexicity_sequence(self, x):
+    def perplexicity_sequence_continues(self, x):
         """
         x - sequence of word ids
         """
@@ -180,9 +185,69 @@ class Job(ComponentsLoader):
 
 
         return perplexicity
+
+
+    def logProb(self, word, context):
+
+        
+        predictions = self.model.predict(np.array([context]))
+        prob = predictions[0][-1][word]
+        return -np.log2(prob);
+
+    def perplexicity_sequence(self, x):
+        """
+        x - sequence of word ids
+        """
+        X = x[:-1]
+        y = x[1:]
+
+        predictions = self.model.predict(np.array([X]))
+
+        try:
+            self._n = int(self.params["model"]["depth"])
+        except:
+            self._n = 5
+
+        self._n = 10
+        unknowns = 0
+ 
+        e = 0.0
+        for i in range(self._n - 1, len(x)):
+            context = x[i - self._n + 1:i]
+            token = x[i]
+            if token == 0 or 0 in context or token == 460:
+                unknowns += 1
+                continue
+            cur = self.logProb(token, context)
+            e += cur
+
+            sent = ' '.join(self.mapper.idx_to_sequence( x[i - self._n + 1:i+1] ))
+
+            meanProb = e / float(i - (self._n - 2 + unknowns))
+            ppx = np.exp2(meanProb)
+
+            print "%d/%d - %s\nCur - %0.2f\tPPX - %0.2f" % (i, len(x),sent, np.exp2(cur),ppx)
+
+        try:
+            meanProb = e / float(len(x) - (self._n - 1 + unknowns))
+        except:
+            meanProb = 15
+        
+        # totalProb = 0.0
+        #
+        # for i, wordPreds in enumerate(predictions[0]):
+        #     pWord = wordPreds[(y[i])]
+        #     totalProb -= np.log2(pWord)
+        #
+        # meanProb = totalProb / float(len(X))
+        
+        perplexicity = np.exp2(meanProb)
+
+
+        return perplexicity
  
 
-    def predict_sentance_words(self, sentance):
+    def predict_sentance(self, sentance):
 
         sentances = [sentance.split(" ")]
         X, Y = self.mapper.processSentances(sentances)
@@ -212,7 +277,7 @@ class Job(ComponentsLoader):
 
         return outMap
 
-    def predict_sentance(self, sentance):
+    def predict_sentance_classify(self, sentance):
 
         sentances = [sentance.split(" ")]
         X, Y = self.mapper.processSentances(sentances)