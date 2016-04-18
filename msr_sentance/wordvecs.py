505591458f52671fcadbb0cbbde75f8c00cbc9ec
Rohan Raja <rohanraja9@gmail.com>
2016-04-18 16:11:07 +0530

    Added binary reading

--- msr_sentance/wordvecs.py
+++ msr_sentance/wordvecs.py
@@ -1,40 +1,42 @@
 import numpy as np
+from gensim.models.word2vec import Word2Vec
 
 import sys
 
 try:
     fname = sys.argv[1]
 except:
-    fname = "out.txt"
+    fname = "out.bin"
 
-f = open(fname, 'r')
-
-lines = f.readlines()
-lines = lines[1:]
-
-f.close()
-
-wordVecs = {}
-
-for line in lines:
-
-    try:
-        words = line.split()
-
-        w = words[0]
-        words = words[1:]
-
-        vec = np.array(words, dtype = float)
-
-        wordVecs[w] = vec
-    except:
-        print "Error"
-
-
+# f = open(fname, 'r')
+#
+# lines = f.readlines()
+# lines = lines[1:]
+#
+# f.close()
+#
+# wordVecs = {}
+#
+# for line in lines:
+#
+#     try:
+#         words = line.split()
+#
+#         w = words[0]
+#         words = words[1:]
+#
+#         vec = np.array(words, dtype = float)
+#
+#         wordVecs[w] = vec
+#     except:
+#         print "Error"
+#
+#
+wordVecs = Word2Vec.load_word2vec_format(fname, binary=True, unicode_errors='ignore')
 def getWvec(word):
 
     try:
         return wordVecs[word]
     except:
-        return wordVecs['a']
+        return wordVecs['the']
 
